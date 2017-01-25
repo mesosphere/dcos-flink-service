09de0fc050694732eca548e4801a9346e0f2b3b1
Vijay Srinivasaraghavan <vijayaraghavan.srinivasaraghavan@emc.com>
2017-01-24 17:09:39 -0800

    FLINK-5537 Added SSL configuration for DCOS/Flink

--- container/appmaster/runit/init.sh
+++ container/appmaster/runit/init.sh
@@ -10,5 +10,27 @@ export LIBPROCESS_PORT="$PORT4"
 
 export FLINK_UI_WEB_PROXY_BASE="/service/${DCOS_SERVICE_NAME}"
 
+# validate base64 encoded keystore and truststroe
+if [[ "${FLINK_SSL_ENABLED}" == true ]]; then
+	KEYDIR=`mktemp -d`
+	trap "rm -rf $KEYDIR" EXIT
+
+	echo "${FLINK_SSL_KEYSTOREBASE64}" | base64 -d > "$KEYDIR/flink.keystore"
+	ALIAS=$(keytool -list -keystore "$KEYDIR/flink.keystore" -storepass "${FLINK_SSL_KEYSTOREPASSWORD}" | grep PrivateKeyEntry | cut -d, -f1 | head -n1)
+	if [[ -z "${ALIAS}" ]]; then
+		echo "Cannot find private key in keystore"
+		exit 1
+	fi
+
+	echo "${FLINK_SSL_TRUSTSTOREBASE64}" | base64 -d > "$KEYDIR/flink.truststore"
+	ALIAS=$(keytool -list -keystore "$KEYDIR/flink.truststore" -storepass "${FLINK_SSL_TRUSTSTOREPASSWORD}" | grep trustedCertEntry | cut -d, -f1 | head -n1)
+	if [[ -z "${ALIAS}" ]]; then
+		echo "Cannot find trusted cert entry in keystore"
+		exit 1
+	fi
+
+	rm -rf "$KEYDIR"
+fi
+
 # start service
 exec runsvdir -P /etc/service
