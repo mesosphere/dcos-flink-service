3828c336ceaa13b8dc2311304da8b06cad264f2a
Robin Oh <OhRobin@users.noreply.github.com>
2017-08-04 14:36:51 -0700

    remove upload

--- container/appmaster/cli/dcos-flink/main.go
+++ container/appmaster/cli/dcos-flink/main.go
@@ -8,9 +8,6 @@ import (
 	"gopkg.in/alecthomas/kingpin.v2"
 	"fmt"
 	"log"
-	"strings"
-	"net/http"
-	"io/ioutil"
 )
 
 
@@ -20,7 +17,6 @@ func main() {
 	handleListSection(app)
 	handleJobSection(app)
 	handleRunSection(app)
-	handleUploadSection(app)
 	handleCancelSection(app)
   handleJarsSection(app)
 
@@ -129,42 +125,4 @@ func handleCancelSection(app *kingpin.Application) {
 	cmd := &CancelHandler{}
 	cancel := app.Command("cancel", "Cancel flink job").Action(cmd.runCancel)
 	cancel.Arg("job id", "job id of flink").Required().StringVar(&cmd.cancel)
-}
-
-//upload
-type UploadHandler struct {
-	upload string
-}
-
-func (cmd *UploadHandler) runUpload(c *kingpin.ParseContext) error {
-	//TODO x509 auth instead of https to http change
-	url := client.OptionalCLIConfigValue("core.dcos_url") //TODO this should be a RequiredCLIConfigValue
-	url = strings.Replace(url,"https://", "http://", 1)
-	url = fmt.Sprintf("%s/service/flink/jars/upload", url)
-
-	//create multipart payload
-	payload := strings.NewReader(fmt.Sprintf("------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"jarfile\"; filename=\"%s\"\r\nContent-Type: application/java-archive\r\n\r\n\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--", cmd.upload))
-
-	req, _ := http.NewRequest("POST", url, payload)
-
-	//fetch the Auth token from the main CLI.
-	req.Header.Add("authorization", fmt.Sprintf("token=%s", client.OptionalCLIConfigValue("core.dcos_acs_token")))
-	req.Header.Add("content-type", "multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW")
-
-	res, err := http.DefaultClient.Do(req)
-	if err != nil {
-    fmt.Println("Error: %s\n", err)
-    return nil
-}
-	defer res.Body.Close()
-	body, _ := ioutil.ReadAll(res.Body)
-
-	fmt.Println(string(body))
-	return nil
-}
-
-func handleUploadSection(app *kingpin.Application) {
-	cmd := &UploadHandler{}
-	upload := app.Command("upload", "Upload flink jar to run").Action(cmd.runUpload)
-	upload.Arg("jar file", "jar file to upload").Required().StringVar(&cmd.upload)
 }
